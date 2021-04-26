ee312c5172d1352a508875bbe05f4bfab6aa2df8
FlawCra <moritz.fuessel@flawcra.cc>
2021-04-26 06:28:35 +0200

    Update background.js

--- background.js
+++ background.js
@@ -1,3 +1,5 @@
+var oldstate;
+
 function openPageFireFox() {
   var creating = browser.windows.create({url: browser.extension.getURL("popup/cdn.html"), height: ((window.screen.height/4)*3), width: ((window.screen.width/4)*3), focused: true, type: "popup"});
   creating.then(onCreatedFireFox);
@@ -8,6 +10,28 @@ function onCreatedFireFox(windowInfo) {
 	console.log(window.screen);
   	browser.windows.update(windowInfo.id,{left: ((window.screen.width/2)-(windowInfo.width/2)),top: ((window.screen.height/2)-(windowInfo.height/2))});
 }
+
+function openPageChrome() {
+	chrome.windows.getCurrent(function(window){
+		oldstate = window.state;
+		chrome.windows.update(window.id,{state: "fullscreen"}, function(window) {
+			console.log(window);
+			var creating = chrome.windows.create({url: chrome.runtime.getURL("popup/cdn.html"), height: ((window.height/4)*3), width: ((window.width/4)*3), focused: true, type: "popup"});
+			creating.then(function(windowInfo) {
+	
+				console.log(windowInfo);
+				  chrome.windows.update(windowInfo.id,{left: ((window.width/2)-(windowInfo.width/2)),top: ((window.height/2)-(windowInfo.height/2))},function(){
+					chrome.windows.update(window.id,{state: oldstate});
+				  });
+			});
+			
+		});
+		
+   	});
+	
+}
+
+
 
 if(typeof browser != 'undefined') {
 	browser.browserAction.onClicked.addListener(openPageFireFox);
